c7636398d56b4aa5eec6bf333f19b01a709560ad
luluct <luluct@163.com>
2020-09-09 17:32:05 +0800

    -l-

--- app/Http/Controllers/Vlog/ArticleController.php
+++ app/Http/Controllers/Vlog/ArticleController.php
@@ -4,19 +4,23 @@ namespace App\Http\Controllers\Vlog;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Http\Services\ArticleService;
+use App\Services\ArticleService;
 
 class ArticleController extends Controller {
 
 	
     public function index(Request $request){
         $params = $request->all();
+		
+	print_r($params);
 
         $articleService = new ArticleService();
 
         $detailData = $articleService->getArticleById($params);
 
-        $viewData = [
+	print_r($detailData);
+        exit;
+	$viewData = [
             'article' => $detailData,
         ];
         return view('vlog.detail', $viewData);
